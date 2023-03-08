4da37344f0cf229e6fb3e0ffd8bbc315bf64a6d9
kite3 <lijincheng1@xinli001.com>
2023-03-09 00:14:19 +0800

    完善日志打印

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,4 +1,25 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  console.table(JSON.parse(message).properties)
+  const data = JSON.parse(message)
+
+  let target = {
+    '----------神策埋点----------': '----------神策埋点----------',
+    事件: data['event'],
+    distinct_id: data.distinct_id || ' ',
+    login_id: data.login_id || ' '
+  }
+  let properties = data.properties
+  for (let key in properties) {
+    if (typeof properties[key] === 'string') {
+      if (properties[key].trim() === '') {
+        target[key] = ' '
+      } else {
+        target[key] = properties[key]
+      }
+    } else {
+      target[key] = JSON.stringify(properties[key])
+    }
+  }
+
+  console.table(target)
   sendResponse('get!')
 })
