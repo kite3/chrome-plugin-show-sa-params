669ada3a92c6b921bed088ebaa501d996986c323
kite3 <lijincheng1@xinli001.com>
2023-03-08 23:25:00 +0800

    fix: 不要用window.atob对bas64进行解码，会导致中！文！乱！码！！

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,7 +1,4 @@
-console.info('======contentScript======')
-
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  console.log('background-script发来的消息')
   console.table(JSON.parse(message).properties)
   sendResponse('get!')
 })
